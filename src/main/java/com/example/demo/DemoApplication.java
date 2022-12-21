9a56c9ba5410ef380502b900274de500ec3c58b9
mtorreshe <mtorreshe@ipn.mx>
2022-12-21 13:54:22 -0600

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,7 +12,7 @@ public class DemoApplication {
 
     @RequestMapping("/")
     String home() {
-        return "Hello World!";
+        return "Hola mundo desde mi repo con MS! Marco";
     }
 
     public static void main(String[] args) {
